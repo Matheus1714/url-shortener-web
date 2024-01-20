6f101b4ec8e7ddfce0c915fa9f330579d4fb2a2d
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-20 19:43:17 -0300

    create card main component

---
+++ src/components/CardMain.vue
@@ -0,0 +1,39 @@
+<template>
+    <main>
+        <CardInput placeholder="URL Here"/>
+        <CardButton title="Generate Shortener"/>
+    
+        <DivLine />
+    
+        <CardInput disabled showCopyIcon/>
+        <CardButton title="Go To URL"/>
+    
+        <DivLine />
+    </main>
+</template>
+
+<script>
+import DivLine from './DivLine.vue';
+import CardButton from './CardButton.vue'
+import CardInput from './CardInput.vue'
+
+export default {
+    components: {
+        DivLine,
+        CardButton,
+        CardInput
+    }
+}
+</script>
+
+<style>
+main{
+    width: 100%;
+    
+    margin: 1rem 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+}
+</style>
