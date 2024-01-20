222cc3428e18248752946096c2944ff30b895d75
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-20 19:43:39 -0300

    create div line component

---
+++ src/components/DivLine.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="line"></div>
+</template>
+
+<style>
+    .line{
+        height: 0.25rem;
+        width: 63%;
+        background: var(--white);
+
+        border-radius: 9999px;
+    }
+</style>
