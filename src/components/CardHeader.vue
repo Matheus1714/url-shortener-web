81d6c477b0a75fb6a2748c714f56fa402115ab27
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-20 19:43:02 -0300

    create card header component

---
+++ src/components/CardHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <ClipIcon />
+        <h1>URL Shortener</h1>
+    </header>
+</template>
+
+<script>
+import ClipIcon from './ClipIcon.vue';
+
+export default {
+  components: {
+    ClipIcon,
+  }
+}
+</script>
+
+<style>
+    header{
+        text-align: center;
+    }
+</style>
