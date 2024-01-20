311367771474a59c261c06681ffae6a95c97ad8e
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-20 19:42:45 -0300

    create card button component

---
+++ src/components/CardButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button>{{ title }}</button>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style lang="scss">
+    button{
+        border: none;
+        text-align: center;
+        text-decoration: none;
+
+        background: var(--button-color);
+        font-weight: 700;
+
+        width: 100%;
+        border-radius: 4px;
+        padding: 0.25rem;
+
+        transition: all 0.2s;
+
+        &:hover{
+            background: var(--purple-light);
+        }
+        &:active{
+            background: var(--black-light);
+        }
+    }
+</style>
