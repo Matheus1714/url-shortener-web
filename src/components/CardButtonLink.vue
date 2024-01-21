83c31fa9991dad508262f5c60b1115271fdea76d
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-20 22:14:57 -0300

    create new component Button

---
+++ src/components/CardButtonLink.vue
@@ -0,0 +1,41 @@
+<template>
+    <button @click="openNewTab()">{{ title }}</button>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        shortUrl: String
+    },
+    methods: {
+        openNewTab(){
+            window.open(this.shortUrl, "_blank");
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    button{
+        border: none;
+        text-align: center;
+        text-decoration: none;
+
+        background: var(--button-color);
+        font-weight: 700;
+
+        width: 100%;
+        border-radius: 4px;
+        padding: 0.25rem;
+
+        transition: all 0.2s;
+
+        &:hover{
+            background: var(--purple-light);
+        }
+        &:active{
+            background: var(--black-light);
+        }
+    }
+</style>
